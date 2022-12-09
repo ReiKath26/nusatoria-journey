11ad4d9c5781b0454f34ca27bbe18b1defc25cae
Kathleen Febiola <kathy.febiola02@gmail.com>
2022-12-09 22:46:42 +0700

    Home page set up

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    public enum AudioTypes {sfx, music}
+    public AudioTypes audioType;
+
+    
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
